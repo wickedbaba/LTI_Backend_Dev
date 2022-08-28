cf501d6bdfb1b8edceccd24160c148ad42c7d3f7
Aryaman <54187786+wickedbaba@users.noreply.github.com>
2022-08-29 00:12:36 +0530

    Update myApp.js

--- Module3/myApp.js
+++ Module3/myApp.js
@@ -11,7 +11,6 @@ const personSchema = new Schema({
 
 const Person = mongoose.model('Person',personSchema);
 
-// mongodb+srv://lockem:<password>@cluster0.9dnn2d4.mongodb.net/?retryWrites=true&w=majority
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
