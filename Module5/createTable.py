ad4be37e5eeba9a09f9054c0a070899fd3d051a0
wickedbaba <aryamansinghranaasr@gmail.com>
2023-02-16 17:41:34 +0530

    Learning DynamoDB

---
+++ Module5/createTable.py
@@ -0,0 +1,47 @@
+import boto3
+
+
+def create_movie_table(dynamodb=None):
+    if not dynamodb:
+
+        dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
+
+    table = dynamodb.create_table(
+        TableName='Movies',
+        KeySchema=[
+            {
+                'AttributeName': 'year',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'title',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'year',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'title',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    movie_table = create_movie_table()
+    print("Table Name:", movie_table.table_name)
+    print("Table status:", movie_table.table_status)
+    print("Table ARN:", movie_table.table_arn)
+    client = boto3.client('dynamodb')
+    respons = client.describe_table(TableName='Movies')
+    print("Table desc:", respons)
---
+++ Module5/readme.md
@@ -0,0 +1,1 @@
+# learning DynamoDB using Python.
